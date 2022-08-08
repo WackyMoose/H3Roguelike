d7a554c345e7a1df1485ce9b4f9c36708c9192c1
Victor Hansen <kingodk@outlook.dk>
2022-08-08 10:58:59 +0200

    Added frametime

--- H3Roguelike/Program.cs
+++ H3Roguelike/Program.cs
@@ -9,7 +9,10 @@ while (!Raylib.WindowShouldClose())
     Raylib.BeginDrawing();
     Raylib.ClearBackground(Color.WHITE);
 
-    Raylib.DrawText("Hello, world!", 12, 12, 20, Color.BLACK);
+    Raylib.DrawText("Hello world!", 12, 12, 20, Color.BLACK);
+
+    var frametime = Raylib.GetFrameTime();
+    Raylib.DrawText($"Frame time: {frametime / 1000.0f}ms ({1.0f / frametime} FPS)", 12, 36, 20, Color.BLACK);
 
     Raylib.EndDrawing();
 }
