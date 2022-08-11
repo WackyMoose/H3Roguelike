cc3466fef6d59e5867da36044b0b4a9ab6d5dd5f
PierreNS <pierreskinnerup@gmail.com>
2022-08-11 10:32:28 +0200

    [Merge] Fixed merge conflict in TestGame.cs

--- H3Roguelike/TestGame.cs
+++ H3Roguelike/TestGame.cs
@@ -10,7 +10,13 @@ internal class TestGame : Game
 
     public override void Start()
     {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        var spriteCoords = new Vector2(4, 0);
+
+        testEntity = new TestEntity(spriteCoords);
+        testEntity.Scale = new Vector2(64, 64);
+
+        testEntity.Position = new Vector2(100, 100);
     }
 
     public override void Update(float deltaTime)
