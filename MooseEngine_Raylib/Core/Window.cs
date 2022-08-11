e7708d2841df5f76d9c6e658f1d429424792095f
Victor Krogh Hansen <kingodk@outlook.dk>
2022-08-11 10:55:33 +0200

    Added Window class (and WindowSpecification struct).

---
+++ MooseEngine_Raylib/Core/Window.cs
@@ -0,0 +1,28 @@
+﻿using Raylib_cs;
+
+namespace MooseEngine.Core;
+
+public class Window
+{
+    private WindowSpecification _specification;
+
+    public Window(WindowSpecification specification)
+    {
+        _specification = specification;
+    }
+
+    public void Initialize()
+    {
+        Raylib.InitWindow(_specification.Width, _specification.Height, _specification.Title);
+    }
+
+    public void Shutdown()
+    {
+        Raylib.CloseWindow();
+    }
+
+    public void Update()
+    {
+        Raylib.SwapScreenBuffer();
+    }
+}
