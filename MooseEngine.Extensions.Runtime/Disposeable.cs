a421084209d94cd2e07a9c4204c9b7f854859f16
Victor Krogh Hansen <kingodk@outlook.dk>
2022-08-11 10:31:37 +0200

    Added Throw class. Added Disposeable class.

---
+++ MooseEngine.Extensions.Runtime/Disposeable.cs
@@ -0,0 +1,43 @@
+﻿namespace MooseEngine.Extensions.Runtime;
+
+public class Disposeable : IDisposable
+{
+    private bool _disposed;
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    private void Dispose(bool disposing)
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            DisposeManagedState();
+        }
+
+        FreeUnmanagedResources();
+
+        ClearLargeFields();
+
+        _disposed = true;
+    }
+
+    protected virtual void DisposeManagedState()
+    {
+    }
+
+    protected virtual void FreeUnmanagedResources()
+    {
+    }
+
+    protected virtual void ClearLargeFields()
+    {
+    }
+}
---
+++ MooseEngine.Extensions.Runtime/Throw.cs
@@ -0,0 +1,12 @@
+﻿namespace MooseEngine.Extensions.Runtime;
+
+public static class Throw
+{
+    public static void IfNull(object? obj, string message)
+    {
+        if (obj == null)
+        {
+            throw new Exception(message);
+        }
+    }
+}
