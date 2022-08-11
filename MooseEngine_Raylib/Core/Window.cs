52fb525491024c7eabac6a4efd57d6a9f3c93ed0
Victor Krogh Hansen <kingodk@outlook.dk>
2022-08-11 11:47:29 +0200

    Added Width and Height getters.

--- MooseEngine_Raylib/Core/Window.cs
+++ MooseEngine_Raylib/Core/Window.cs
@@ -6,6 +6,9 @@ public class Window
 {
     private WindowSpecification _specification;
 
+    public int Width { get { return _specification.Width; } }
+    public int Height { get { return _specification.Height; } }
+
     public Window(WindowSpecification specification)
     {
         _specification = specification;
