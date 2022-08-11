59e96125f95a466f85dc3c197fa03ba5de7ddf5b
Victor Krogh Hansen <kingodk@outlook.dk>
2022-08-11 10:53:45 +0200

    Added Singleton method

--- MooseEngine.Extensions.Runtime/Throw.cs
+++ MooseEngine.Extensions.Runtime/Throw.cs
@@ -2,6 +2,14 @@
 
 public static class Throw
 {
+    public static void IfSingletonExists(object? instance, string message)
+    {
+        if (instance != default)
+        {
+            throw new Exception(message);
+        }
+    }
+
     public static void IfNull(object? obj, string message)
     {
         if (obj == null)
