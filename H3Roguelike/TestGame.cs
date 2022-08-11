e0edc1e5dc08ff5f2bb55c779ec674d29a8631cf
Victor Krogh Hansen <kingodk@outlook.dk>
2022-08-11 12:34:07 +0200

    Testing Noise

--- H3Roguelike/TestGame.cs
+++ H3Roguelike/TestGame.cs
@@ -1,5 +1,6 @@
 ﻿using MooseEngine.Core;
 using MooseEngine.Scenes;
+using Raylib_cs;
 using System.Numerics;
 
 namespace H3Roguelike;
@@ -7,6 +8,7 @@ namespace H3Roguelike;
 internal class TestGame : IGame
 {
     private Scene? _scene;
+    private Texture2D _texture;
 
     public void Initialize()
     {
@@ -15,6 +17,16 @@ internal class TestGame : IGame
         var entity = new TestEntity(new Vector2(5, 0));
         entity.Scale = new Vector2(64, 64);
         entity.Position = new Vector2(128, 192);
+
+
+        //var noise = SimplexNoise.Noise.Calc2D(100, 100, 1.0f);
+        //var image = Raylib.GenImageCellular(500, 500, 100);
+
+        var app = Application.Instance;
+        var window = app.Window;
+
+        var image = Raylib.GenImageWhiteNoise(window.Width, window.Height, 0.1f);
+        _texture = Raylib.LoadTextureFromImage(image);
 
         _scene?.Add(entity);
     }
@@ -27,6 +39,9 @@ internal class TestGame : IGame
 
     public void Update(float deltaTime)
     {
-        _scene?.UpdateRuntime(deltaTime);
+        Renderer.Begin();
+        Renderer.RenderTexture(_texture, 100, 100);
+        Renderer.End();
+        //_scene?.UpdateRuntime(deltaTime);
     }
 }
---
+++ MooseEngine_Raylib/Utilities/Noise.cs
@@ -0,0 +1,9 @@
+﻿namespace MooseEngine.Utilities;
+
+public static class Noise
+{
+    public static float[,] PerlinNoise2D()
+    {
+        return new float[0, 0];
+    }
+}
