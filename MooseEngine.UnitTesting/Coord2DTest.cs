e69631d8fa452d4829dcefd546e8376ec5f4a131
Victor Hansen <kingodk@outlook.dk>
2022-08-18 11:43:44 +0200

    Added MooseEngineIoCTest for Autofac tests. Added Coords2DTest for testing operators on Coords2D struct.

---
+++ MooseEngine.UnitTesting/Coord2DTest.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MooseEngine.Utilities;
+
+namespace MooseEngine.UnitTesting;
+
+[TestClass]
+public class Coord2DTest
+{
+    [TestMethod]
+    public void Coord2D_2_Plus_2_Add_Operator()
+    {
+        var coord1 = new Coords2D(2, 2);
+        var coord2 = new Coords2D(2, 2);
+        var expected = new Coords2D(4, 4);
+
+        var result = coord1 + coord2;
+
+        var x = result.X == expected.X;
+        var y = result.Y == expected.Y;
+        Assert.IsTrue(x && y);
+    }
+
+    [TestMethod]
+    public void Coord2D_10_Plus_31_Add_Operator()
+    {
+        var coord1 = new Coords2D(10, 31);
+        var coord2 = new Coords2D(31, 10);
+        var expected = new Coords2D(41, 41);
+
+        var result = coord1 + coord2;
+
+        var x = result.X == expected.X;
+        var y = result.Y == expected.Y;
+        Assert.IsTrue(x && y);
+    }
+
+    [TestMethod]
+    public void Coord2D_2_Plus_2_Minus_Operator()
+    {
+        var coord1 = new Coords2D(2, 2);
+        var coord2 = new Coords2D(2, 2);
+        var expected = new Coords2D(0, 0);
+
+        var result = coord1 - coord2;
+
+        var x = result.X == expected.X;
+        var y = result.Y == expected.Y;
+        Assert.IsTrue(x && y);
+    }
+
+    [TestMethod]
+    public void Coord2D_10_Plus_31_Minus_Operator()
+    {
+        var coord1 = new Coords2D(10, 31);
+        var coord2 = new Coords2D(31, 10);
+        var expected = new Coords2D(-21, 21);
+
+        var result = coord1 - coord2;
+
+        var x = result.X == expected.X;
+        var y = result.Y == expected.Y;
+        Assert.IsTrue(x && y);
+    }
+}
---
+++ MooseEngine.UnitTesting/MooseEngineIoCTest.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MooseEngine.Core;
+using MooseEngine.DependencyInjection;
+
+namespace MooseEngine.UnitTesting;
+
+[TestClass]
+public class MooseEngineIoCTest
+{
+    [TestMethod]
+    public void Is_Application_Registered()
+    {
+        var containerBuilder = new ContainerBuilder();
+
+        containerBuilder.RegisterModule<CoreModule>();
+
+        var container = containerBuilder.Build();
+
+        var scope = container.BeginLifetimeScope();
+
+        var isApplicationRegistered = container.IsRegistered<IApplication>();
+        Assert.IsTrue(isApplicationRegistered);
+
+        scope.Dispose();
+    }
+}
