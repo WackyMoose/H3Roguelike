3d1d174b23fd0fc54724889d22076fe28a07aa8b
Victor Hansen <kingodk@outlook.dk>
2022-08-22 13:48:49 +0200

    Fixed nullable instance warning

--- MooseEngine_Raylib/Core/Application.cs
+++ MooseEngine_Raylib/Core/Application.cs
@@ -17,20 +17,23 @@ public interface IApplication : IDisposable
 public sealed class Application : Disposeable, IApplication
 {
     private static Application? s_Instance;
-    public static Application? Instance { get { return s_Instance; } }
-
-    private readonly ApplicationOptions _options;
+    public static Application Instance
+    {
+        get
+        {
+            Throw.IfNull(s_Instance, "No Application instance exists!");
+            return s_Instance!;
+        }
+    }
 
     public Application(ApplicationOptions options, IGame game, IWindow window, IRenderer renderer, ISceneFactory sceneFactory)
     {
-        Throw.IfSingletonExists(Instance, "Application already exists!");
+        Throw.IfSingletonExists(s_Instance, "Application already exists!");
         s_Instance = this;
 
         Game = game;
         Window = window;
         Renderer = renderer;
-        _options = options;
-
         SceneFactory = sceneFactory;
     }
 
@@ -42,30 +45,29 @@ public sealed class Application : Disposeable, IApplication
     public void Initialize()
     {
         Throw.IfNull(Window, "No Window instance.");
+        Throw.IfNull(Renderer, "No Renderer instance.");
         Throw.IfNull(Game, "No Game instance has been set.");
 
         Window.Initialize();
-
         Renderer.Initialize();
     }
 
     protected override void DisposeManagedState()
     {
         Renderer.Shutdown();
-
-        Window?.Shutdown();
+        Window.Shutdown();
     }
 
     public void Run()
     {
-        Game?.Initialize();
+        Game.Initialize();
 
         while (Window.IsRunning)
         {
             var deltaTime = Raylib.GetFrameTime();
-            Game?.Update(deltaTime);
+            Game.Update(deltaTime);
         }
 
-        Game?.Uninitialize();
+        Game.Uninitialize();
     }
 }
