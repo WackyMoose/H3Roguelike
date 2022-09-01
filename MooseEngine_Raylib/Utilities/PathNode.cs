93320c7ce0a05a4561618dce3f7bb8017476fdda
PierreNS <pierreskinnerup@gmail.com>
2022-09-01 12:21:41 +0200

    [Pathfinding] Create node and pathfinder

---
+++ MooseEngine_Raylib/Utilities/PathNode.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace MooseEngine.Utilities
+{
+    public class PathNode
+    {
+        public Coords2D Position;
+        public float G;
+        public float H;
+        public float F;
+        public PathNode? Parent;
+
+        public PathNode(Coords2D position, float g, float h, float f, PathNode? parent)
+        {
+            Position = position;
+            G = g;
+            H = h;
+            F = f;
+            Parent = parent;
+        }
+    }
+}
---
+++ MooseEngine_Raylib/Utilities/Pathfinder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MooseEngine.Utilities
+{
+    public class Pathfinder
+    {
+
+
+        public Vector2[] GetPath(Vector2 startPos, Vector2 goalPos) 
+        {
+            //
+
+            return new Vector2[0];
+        }
+    }
+}
