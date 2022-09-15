715fa7ea2761a56eb9a3fc854e6237cc2c702c2e
Victor Hansen <kingodk@outlook.dk>
2022-09-15 14:32:03 +0200

    Added UserController

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+public class User
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public DateTime Created { get; set; }
+    public DateTime Updated { get; set; }
+}
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+	public UserController()
+	{
+	}
+
+    [HttpGet]
+    public Task<User> GetByIdAsync(int id)
+    {
+        return Task.FromResult(new User
+        {
+            Id = id,
+            Name = "Kurt",
+            Created = DateTime.Now,
+            Updated = DateTime.Now
+        });
+    }
+
+    [HttpPost]
+    public Task<User> AddUser(User user)
+    {
+        return Task.FromResult(user);
+    }
+}
