f2038c411ca109e9f7967b88b89c8d8139b45875
PierreNS <pierreskinnerup@gmail.com>
2022-09-29 13:12:40 +0200

    [Lights] Added lights to orc camps.

--- GameV1/RogueliteGame.cs
+++ GameV1/RogueliteGame.cs
@@ -295,7 +295,7 @@ internal class RogueliteGame : IGame
         townLights.Position = new Vector2(51, 50) * Constants.DEFAULT_ENTITY_SIZE;
         itemLayer?.AddEntity(townLights);
 
-        for (int i = 0; i < 128; i++)
+        for (int i = 0; i < WorldGenerator._structurePositions.Count; i++)
         {
             // Color(128, 128 - 48, 128 - 96, 255)
             var color = new Color(
@@ -303,7 +303,7 @@ internal class RogueliteGame : IGame
                 144 + Randomizer.RandomInt(-32, 32),
                 128 + Randomizer.RandomInt(-32, 32), 255);
             var light = new LightSource(Randomizer.RandomInt(3, 12) * Constants.DEFAULT_ENTITY_SIZE, color, 1000, 100, "Camp fire", new Coords2D(9, 8), Color.White);
-            light.Position = new Vector2(Randomizer.RandomInt(0, 200), Randomizer.RandomInt(0, 200)) * Constants.DEFAULT_ENTITY_SIZE;
+            light.Position = WorldGenerator._structurePositions[i] + new Vector2((3 * Constants.DEFAULT_ENTITY_SIZE), (3 * Constants.DEFAULT_ENTITY_SIZE));
             //itemLayer?.Add(light);
             scene.TryPlaceEntity((int)EntityLayer.Items, light, light.Position, (int)EntityLayer.Creatures, (int)EntityLayer.NonWalkableTiles);
         }
