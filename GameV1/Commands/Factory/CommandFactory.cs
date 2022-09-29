7368e9d00ce7408b47ecb6ef586654a0ee3ee556
Michael Schmidt Nissen <michaelschmidtnissen@gmail.com>
2022-09-29 13:05:56 +0200

    CommandFactory update

--- GameV1/Commands/Factory/CommandFactory.cs
+++ GameV1/Commands/Factory/CommandFactory.cs
@@ -86,9 +86,11 @@ namespace GameV1.Commands.Factory
 
                     for (int i = 0; i < 10; i++)
                     {
-                        if (inputs.Contains((InputOptions) i + 1))
+                        var slotIndex = i == 0 ? 9 : i - 1;
+
+                        if (inputs.Contains((InputOptions) i))
                         {
-                            return new PickUpItemIndex(scene, (ICreature)entity, i + 1);
+                            return new PickUpItemIndex(scene, (ICreature)entity, slotIndex);
                         }
                     }
                 }
